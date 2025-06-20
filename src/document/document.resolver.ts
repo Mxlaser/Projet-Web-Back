c7707b103ac3cb3f898d916fae66199073d21181
Nikola.Z <nzivkovic@eig.fr>
2025-06-20 11:22:24 +0200

    commentaire commit

--- src/document/document.resolver.ts
+++ src/document/document.resolver.ts
@@ -3,7 +3,7 @@ import { DocumentService } from './document.service';
 import { CreateDocumentInput } from './dto/create-document.input';
 import { Document } from './entities/document.entity';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
-// import { RolesGuard } from '../auth/guards/roles.guard';
+import { RolesGuard } from '../auth/guards/roles.guard';
 import { UseGuards } from '@nestjs/common';
 import { CurrentUser } from '../auth/decorators/current-user.decorator';
 
@@ -28,7 +28,7 @@ export class DocumentResolver {
   }
 
   @Mutation(() => Boolean)
-  // @UseGuards(JwtAuthGuard, RolesGuard)
+  @UseGuards(JwtAuthGuard, RolesGuard)
   async deleteDocument(@Args('id') id: string, @CurrentUser() user: any) {
     return this.documentService.deleteDocument(id, user.userId, user.role);
   }
